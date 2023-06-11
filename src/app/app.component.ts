2af07f342df7eb07f45f3a7853f051dae2b9e74e
saga-r15 <sagarvanshiv31@gmail.com>
2023-06-11 19:48:58 +0530

    First Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todaydemo';
+export class AppComponent 
+{
+  title = 'MarvellousRestaurentApp';
 }
